b50e96673e637e60ec8350eb7e6cd74feb245729
RoddyZ <roddy.zamora@epn.edu.ec>
2021-06-17 11:06:19 -0500

    API REST

---
+++ WebAPI_REST/Data/WebAPI_RESTContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_REST.Models;
+
+namespace WebAPI_REST.Data
+{
+    public class WebAPI_RESTContext : DbContext
+    {
+        public WebAPI_RESTContext (DbContextOptions<WebAPI_RESTContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI_REST.Models.Book> Book { get; set; }
+    }
+}
---
+++ WebAPI_REST/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_REST.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public int Pages { get; set; }
+
+
+    }
+}
